e4e126321f3af704eb463fae8f12d197ede958c9
Augusto Ayub <augustoayub85@gmail.com>
2023-09-29 17:01:19 -0300

    Add project files.

---
+++ AlbionStaticDataLib.Tests/Input/Localization/Models/LocalizationTests.cs
@@ -0,0 +1,19 @@
+﻿using AlbionStaticDataLib.Input.Localization.Models;
+using System.Text.Json;
+
+namespace AlbionStaticDataLib.Tests.Input.Localization.Models
+{
+    [TestClass]
+    public class LocalizationTests
+    {
+        [TestMethod]
+        public async Task TestDeserialization()
+        {
+            using HttpClient client = new HttpClient();
+            string json = await client.GetStringAsync(JsonUrl.Localization);
+            ItemLocalization[] items = JsonSerializer.Deserialize<ItemLocalization[]>(json);
+            Assert.IsNotNull(items);
+            Assert.IsTrue(items.Length > 0);
+        }
+    }
+}
---
+++ AlbionStaticDataLib.Tests/JsonUrl.cs
@@ -0,0 +1,7 @@
+﻿namespace AlbionStaticDataLib.Tests
+{
+    internal static class JsonUrl
+    {
+        public const string Localization = "https://raw.githubusercontent.com/ao-data/ao-bin-dumps/master/formatted/items.json";
+    }
+}
