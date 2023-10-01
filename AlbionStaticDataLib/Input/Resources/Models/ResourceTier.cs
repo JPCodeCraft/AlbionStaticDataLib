a0dddc578b61ee3ad9ab60ab6ea7082789157e19
Augusto Ayub <augustoayub85@gmail.com>
2023-10-01 03:53:47 -0300

    cleanup

--- AlbionStaticDataLib/Input/Resources/Models/ResourceTier.cs
+++ AlbionStaticDataLib/Input/Resources/Models/ResourceTier.cs
@@ -1,18 +1,17 @@
-﻿using System.Text.Json.Serialization;
-using AlbionStaticDataLib.Input.Resources.Converters;
-
-namespace AlbionStaticDataLib.Input.Resources.Models
+﻿namespace AlbionStaticDataLib.Input.Resources.Models
 {
     public class ResourceTier
     {
         [JsonPropertyName("@value")]
-        [JsonConverter(typeof(StringToLongConverter))]
-        public long Value { get; set; }
+        [JsonConverter(typeof(StringToIntConverter))]
+        public int Value { get; set; }
 
         [JsonPropertyName("@resourcevalue")]
-        public string Resourcevalue { get; set; }
+        [JsonConverter(typeof(StringToDoubleConverter))]
+        public double Resourcevalue { get; set; }
 
         [JsonPropertyName("@famevalue")]
-        public string Famevalue { get; set; }
+        [JsonConverter(typeof(StringToDoubleConverter))]
+        public double Famevalue { get; set; }
     }
 }
