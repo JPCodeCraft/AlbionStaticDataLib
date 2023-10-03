5da9df881c6a9c65f45e1b7bcdfe763a23aedaa9
Augusto Ayub <augustoayub85@gmail.com>
2023-10-03 17:08:28 -0300

    doc comments

--- AlbionStaticDataLib/Json/AlbionParser.cs
+++ AlbionStaticDataLib/Json/AlbionParser.cs
@@ -5,15 +5,24 @@ using System.Text;
 
 namespace AlbionStaticDataLib.Json
 {
+    /// <summary>
+    /// Implements the IAlbionParser interface to parse data from Albion's JSON resources.
+    /// </summary>
     public class AlbionParser
     {
         private readonly HttpClient _client;
+
+        /// <summary>
+        /// Initializes a new instance of the AlbionParser class.
+        /// </summary>
+        /// <param name="client">The HttpClient used to send HTTP requests and receive HTTP responses from a resource identified by the specified URL.</param>
 
         public AlbionParser(HttpClient client)
         {
             _client = client;
         }
 
+        /// <inheritdoc/>
         public async Task<LocalizationItem[]> GetLocalizationItems(string url)
         {
             string json = await _client.GetStringAsync(url);
@@ -21,6 +30,8 @@ namespace AlbionStaticDataLib.Json
             var items = await JsonSerializer.DeserializeAsync<LocalizationItem[]>(jsonStream);
             return items;
         }
+
+        /// <inheritdoc/>
         public async Task<ResourcesRoot> GetResources(string url)
         {
             string json = await _client.GetStringAsync(url);
@@ -28,6 +39,8 @@ namespace AlbionStaticDataLib.Json
             var items = await JsonSerializer.DeserializeAsync<ResourcesRoot>(jsonStream);
             return items;
         }
+
+        /// <inheritdoc/>
         public async Task<ItemsRoot> GetItems(string url)
         {
             string json = await _client.GetStringAsync(url);
---
+++ AlbionStaticDataLib/Json/IAlbionParser.cs
@@ -0,0 +1,33 @@
+﻿using AlbionStaticDataLib.Json.Items.Models;
+using AlbionStaticDataLib.Json.Localization.Models;
+using AlbionStaticDataLib.Json.Resources.Models;
+
+namespace AlbionStaticDataLib.Json
+{
+    /// <summary>
+    /// Provides methods to parse data from Albion's JSON resources.
+    /// </summary>
+    public interface IAlbionParser
+    {
+        /// <summary>
+        /// Retrieves and parses item data from the specified JSON URL. Usually AO-DATA Repository is used.
+        /// </summary>
+        /// <param name="url">The URL from which to retrieve JSON data.</param>
+        /// <returns>A Task representing the asynchronous operation, containing the parsed ItemsRoot object.</returns>
+        Task<ItemsRoot> GetItems(string url);
+
+        /// <summary>
+        /// Retrieves and parses localization data from the specified JSON URL. Usually AO-DATA Repository is used.
+        /// </summary>
+        /// <param name="url">The URL from which to retrieve JSON data.</param>
+        /// <returns>A Task representing the asynchronous operation, containing an array of LocalizationItem objects.</returns>
+        Task<LocalizationItem[]> GetLocalizationItems(string url);
+
+        /// <summary>
+        /// Retrieves and parses resource data from the specified JSON URL. Usually AO-DATA Repository is used.
+        /// </summary>
+        /// <param name="url">The URL from which to retrieve resource JSON data.</param>
+        /// <returns>A Task representing the asynchronous operation, containing the parsed ResourcesRoot object.</returns>
+        Task<ResourcesRoot> GetResources(string url);
+    }
+}
